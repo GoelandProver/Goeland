852e6dfd7bcd5a7d9eb3d4f760ebc1e30627c7a0
Lluisaac <isaaclluis@gmail.com>
2024-02-28 13:26:26 +0100

    Updated Coq

--- src/plugins/coq/context.go
+++ src/plugins/coq/context.go
@@ -38,8 +38,6 @@ package coq
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/GoelandProver/Goeland/global"
@@ -87,16 +85,10 @@ func makeContextIfNeeded(root btps.Form, metaList *btps.MetaList) string {
 }
 
 func contextPreamble() string {
-	str := "Add LoadPath \"" + pathToGoelandCoq() + "\" as Goeland.\n"
-	str += "Require Import Goeland.goeland.\n"
+	str := lemmas
 	str += "Parameter goeland_U : Set. (* goeland's universe *)\n"
 	str += "Parameter goeland_I : goeland_U. (* an individual in the universe. *)\n\n"
 	return str
-}
-
-func pathToGoelandCoq() string {
-	path, _ := os.Executable()
-	return filepath.Clean(filepath.Dir(path) + "/")
 }
 
 func getContextFromFormula(root btps.Form) []string {
@@ -171,3 +163,73 @@ func contextualizeMetas(metaList *btps.MetaList) string {
 	}
 	return "Parameters " + strings.Join(result, " ") + " : goeland_U."
 }
+
+var lemmas = `Require Export Classical.
+
+Lemma goeland_notnot : forall P : Prop,
+  P -> (~ P -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_nottrue :
+  (~True -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_and : forall P Q : Prop,
+  (P -> (Q -> False)) -> (P /\ Q -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_or : forall P Q : Prop,
+  (P -> False) -> (Q -> False) -> (P \/ Q -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_imply : forall P Q : Prop,
+  (~P -> False) -> (Q -> False) -> ((P -> Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_equiv : forall P Q : Prop,
+  (~P -> ~Q -> False) -> (P -> Q -> False) -> ((P <-> Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_notand : forall P Q : Prop,
+  (~P -> False) -> (~Q -> False) -> (~(P /\ Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_notor : forall P Q : Prop,
+  (~P -> ~Q -> False) -> (~(P \/ Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_notimply : forall P Q : Prop,
+  (P -> ~Q -> False) -> (~(P -> Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_notequiv : forall P Q : Prop,
+  (~P -> Q -> False) -> (P -> ~Q -> False) -> (~(P <-> Q) -> False).
+Proof. tauto. Qed.
+
+Lemma goeland_ex : forall (T : Type) (P : T -> Prop),
+  (forall z : T, ((P z) -> False)) -> ((exists x : T, (P x)) -> False).
+Proof. firstorder. Qed.
+
+Lemma goeland_all : forall (T : Type) (P : T -> Prop) (t : T),
+  ((P t) -> False) -> ((forall x : T, (P x)) -> False).
+Proof. firstorder. Qed.
+
+Lemma goeland_notex : forall (T : Type) (P : T -> Prop) (t : T),
+  (~(P t) -> False) -> (~(exists x : T, (P x)) -> False).
+Proof. firstorder. Qed.
+
+Lemma goeland_notall : forall (T : Type) (P : T -> Prop),
+  (forall z : T, (~(P z) -> False)) -> (~(forall x : T, (P x)) -> False).
+Proof. intros T P Ha Hb. apply Hb. intro. apply NNPP. exact (Ha x). Qed.
+
+Definition goeland_and_s := fun P Q c h => goeland_and P Q h c.
+Definition goeland_or_s := fun P Q c h i => goeland_or P Q h i c.
+Definition goeland_imply_s := fun P Q c h i => goeland_imply P Q h i c.
+Definition goeland_equiv_s := fun P Q c h i => goeland_equiv P Q h i c.
+Definition goeland_notand_s := fun P Q c h i => goeland_notand P Q h i c.
+Definition goeland_notor_s := fun P Q c h => goeland_notor P Q h c.
+Definition goeland_notimply_s := fun P Q c h => goeland_notimply P Q h c.
+Definition goeland_notequiv_s := fun P Q c h i => goeland_notequiv P Q h i c.
+Definition goeland_ex_s := fun T P c h => goeland_ex T P h c.
+Definition goeland_notall_s := fun T P c h => goeland_notall T P h c.
+`
