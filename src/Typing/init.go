28770d483337163dee2bc1402482590a5620f0b4
Johann Rosain <johann.rosain@gmail.com>
2025-07-23 22:32:14 +0200

    Implementation of the new typing system

--- src/Typing/init.go
+++ src/Typing/init.go
@@ -38,11 +38,122 @@ package Typing
 
 import (
 	"github.com/GoelandProver/Goeland/AST"
+	"github.com/GoelandProver/Goeland/Lib"
 	"sync"
 )
 
 var global_env Env
+var ari_var string
 
 func Init() {
 	global_env = Env{make(map[string]AST.Ty), sync.Mutex{}}
+	ari_var = "number"
+
+	initTPTPNativeTypes()
+}
+
+func initTPTPNativeTypes() {
+	for _, ty := range AST.DefinedTPTPTypes().GetSlice() {
+		AddToGlobalEnv(ty.Symbol(), AST.TType())
+	}
+
+	AddToGlobalEnv(
+		AST.Id_eq.GetName(),
+		AST.MkTyPi(
+			Lib.MkListV("α"),
+			AST.MkTyFunc(AST.MkTyProd(Lib.MkListV(AST.MkTyVar("α"), AST.MkTyVar("α"))), AST.TProp()),
+		),
+	)
+
+	// 1 - Binary predicates
+	recordBinaryProp("$less")
+	recordBinaryProp("$lesseq")
+	recordBinaryProp("$greater")
+	recordBinaryProp("$greatereq")
+
+	// 2 - Binary input arguments
+	recordBinaryInArgs("$sum")
+	recordBinaryInArgs("$difference")
+	recordBinaryInArgs("$product")
+	recordBinaryInArgs("$quotient_e")
+	recordBinaryInArgs("$quotient_t")
+	recordBinaryInArgs("$quotient_f")
+	recordBinaryInArgs("$remainder_e")
+	recordBinaryInArgs("$remainder_t")
+	recordBinaryInArgs("$remainder_f")
+
+	// 3 - $quotient
+	AddToGlobalEnv("$quotient",
+		AST.MkTyPi(
+			Lib.MkListV(ari_var, "rat_or_real"),
+			AST.MkTyFunc(
+				AST.MkTyProd(Lib.MkListV(AST.MkTyVar(ari_var), AST.MkTyVar(ari_var))),
+				AST.MkTyVar("rat_or_real")),
+		))
+
+	// 4 - Unary input arguments
+	recordUnaryInArgs("$uminus")
+	recordUnaryInArgs("$floor")
+	recordUnaryInArgs("$ceiling")
+	recordUnaryInArgs("$truncate")
+	recordUnaryInArgs("$round")
+
+	// 5 - Unary predicates
+	recordUnaryProp("$is_int")
+	recordUnaryProp("$is_rat")
+
+	// 6 - Conversion
+	recordConversion("$to_int", AST.TInt())
+	recordConversion("$to_rat", AST.TRat())
+	recordConversion("$to_real", AST.TReal())
+}
+
+func recordBinaryProp(name string) {
+	AddToGlobalEnv(
+		name,
+		AST.MkTyPi(
+			Lib.MkListV(ari_var),
+			AST.MkTyFunc(AST.MkTyProd(Lib.MkListV(AST.MkTyVar(ari_var), AST.MkTyVar(ari_var))), AST.TProp()),
+		),
+	)
+}
+
+func recordBinaryInArgs(name string) {
+	AddToGlobalEnv(
+		name,
+		AST.MkTyPi(
+			Lib.MkListV(ari_var),
+			AST.MkTyFunc(AST.MkTyProd(Lib.MkListV(AST.MkTyVar(ari_var), AST.MkTyVar(ari_var))), AST.MkTyVar(ari_var)),
+		),
+	)
+}
+
+func recordUnaryInArgs(name string) {
+	AddToGlobalEnv(
+		name,
+		AST.MkTyPi(
+			Lib.MkListV(ari_var),
+			AST.MkTyFunc(AST.MkTyVar(ari_var), AST.MkTyVar(ari_var)),
+		),
+	)
+}
+
+func recordUnaryProp(name string) {
+	AddToGlobalEnv(
+		name,
+		AST.MkTyPi(
+			Lib.MkListV(ari_var),
+			AST.MkTyFunc(AST.MkTyVar(ari_var), AST.TProp()),
+		),
+	)
+}
+
+func recordConversion(name string, out AST.Ty) {
+	AddToGlobalEnv(
+		name,
+		AST.MkTyPi(
+			Lib.MkListV(ari_var),
+			AST.MkTyFunc(AST.MkTyVar(ari_var), out),
+		),
+	)
 }
--- src/Typing/rules.go
+++ src/Typing/rules.go
@@ -38,9 +38,257 @@
 package Typing
 
 import (
+	"fmt"
 	"github.com/GoelandProver/Goeland/AST"
+	"github.com/GoelandProver/Goeland/Glob"
+	"github.com/GoelandProver/Goeland/Lib"
 )
 
+var label = "typing"
+
 func TypeCheck(form AST.Form) bool {
+	return typecheckForm(emptyCon(), form)
+}
+
+func typecheckForm(con Con, form AST.Form) bool {
+	switch f := form.(type) {
+	case AST.Bot, AST.Top:
+		return true
+	case AST.Pred:
+		return checkFunctional(
+			con,
+			f.GetID().GetName(),
+			f.GetTyArgs(),
+			f.GetArgs(),
+			Lib.MkLazy(func() string { return form.ToString() }),
+		)
+
+	case AST.Not, AST.And, AST.Or, AST.Imp, AST.Equ:
+		return typecheckRec(
+			con,
+			f.GetChildFormulas(),
+			Lib.NewList[Lib.Pair[AST.Term, AST.Ty]](),
+			Lib.NewList[AST.Ty](),
+		)
+
+	case AST.All:
+		return typecheckRec(
+			con.addTypedVars(f.GetVarList()),
+			Lib.MkListV(f.GetForm()),
+			Lib.NewList[Lib.Pair[AST.Term, AST.Ty]](),
+			Lib.NewList[AST.Ty](),
+		)
+
+	case AST.Ex:
+		return typecheckRec(
+			con.addTypedVars(f.GetVarList()),
+			Lib.MkListV(f.GetForm()),
+			Lib.NewList[Lib.Pair[AST.Term, AST.Ty]](),
+			Lib.NewList[AST.Ty](),
+		)
+	}
+
+	Glob.Anomaly(
+		label,
+		fmt.Sprintf("%s is not a known internal formula", form.ToString()),
+	)
 	return false
+}
+
+func typecheckTerm(con Con, term AST.Term, ty AST.Ty) bool {
+	switch t := term.(type) {
+	case AST.Var:
+		if !con.contains(t.GetName(), ty) {
+			Glob.Fatal(
+				label,
+				fmt.Sprintf(
+					"Variable %s is either not in the context or should not have type %s\nContext: %s",
+					t.GetName(),
+					ty.ToString(),
+					con.toString(),
+				))
+			return false
+		}
+
+		return typecheckRec(
+			con,
+			Lib.NewList[AST.Form](),
+			Lib.NewList[Lib.Pair[AST.Term, AST.Ty]](),
+			Lib.MkListV(ty),
+		)
+
+	case AST.Fun:
+		return checkFunctional(
+			con,
+			t.GetID().GetName(),
+			t.GetTyArgs(),
+			t.GetArgs(),
+			Lib.MkLazy(func() string { return t.ToString() }),
+		)
+	}
+
+	Glob.Anomaly(
+		label,
+		fmt.Sprintf("Only bound variables and functions should be typechecked, but found %s", term.ToString()),
+	)
+	return false
+}
+
+func typecheckType(con Con, ty AST.Ty) bool {
+	switch nty := ty.(type) {
+	case AST.TyBound:
+		if !con.contains(nty.GetName(), AST.TType()) {
+			Glob.PrintInfo("Context", con.toString())
+			Glob.Fatal(
+				label,
+				fmt.Sprintf(
+					"Variable %s is either not in the context or is not a type variable\nContext: %s",
+					nty.ToString(),
+					con.toString(),
+				))
+			return false
+		}
+
+		return true
+
+	case AST.TyConstr:
+		oty := QueryGlobalEnv(nty.Symbol())
+
+		switch rty := oty.(type) {
+		case Lib.Some[AST.Ty]:
+			args := AST.GetArgsTy(rty.Val)
+			if args.Len() != nty.Args().Len() {
+				Glob.Fatal(
+					label,
+					fmt.Sprintf(
+						"Type constructor %s expects %d arguments, got %d",
+						nty.Symbol(),
+						args.Len(),
+						nty.Args().Len(),
+					),
+				)
+				return false
+			}
+
+			if nty.Args().Empty() {
+				return true
+			}
+
+			return typecheckRec(
+				con,
+				Lib.NewList[AST.Form](),
+				Lib.NewList[Lib.Pair[AST.Term, AST.Ty]](),
+				nty.Args(),
+			)
+
+		case Lib.None[AST.Ty]:
+			Glob.Anomaly(
+				label,
+				fmt.Sprintf("Unknown type %s", nty.ToString()),
+			)
+		}
+	}
+
+	Glob.Anomaly(
+		label,
+		fmt.Sprintf("On typechecking of types: expected atomic type, got %s", ty.ToString()),
+	)
+	return false
+}
+
+func checkFunctional(
+	con Con,
+	name string,
+	tys Lib.List[AST.Ty],
+	args Lib.List[AST.Term],
+	debug_str Lib.Lazy[string],
+) bool {
+	oty := QueryEnvInstance(name, tys)
+	switch ty := oty.(type) {
+	case Lib.Some[AST.Ty]:
+		instantiated_ty := AST.GetArgsTy(ty.Val)
+		terms_checker := buildTermCheckList(
+			debug_str,
+			ty.Val,
+			args,
+			instantiated_ty,
+		)
+		tys.Append(AST.GetOutTy(ty.Val))
+
+		return typecheckRec(con, Lib.NewList[AST.Form](), terms_checker, tys)
+
+	case Lib.None[AST.Ty]:
+		Glob.Fatal(
+			label,
+			fmt.Sprintf("Type of %s not found in the global environment", debug_str.Run()),
+		)
+	}
+	return false
+}
+
+func typecheckRec(
+	con Con,
+	forms Lib.List[AST.Form],
+	typed_terms Lib.List[Lib.Pair[AST.Term, AST.Ty]],
+	tys Lib.List[AST.Ty],
+) bool {
+	calls := []func(chan bool){}
+
+	for _, form := range forms.GetSlice() {
+		loop_form := form
+		calls = append(calls, func(outchan chan bool) {
+			outchan <- typecheckForm(con, loop_form)
+		})
+	}
+
+	for _, typed_term := range typed_terms.GetSlice() {
+		loop_term := typed_term
+		calls = append(calls, func(outchan chan bool) {
+			outchan <- typecheckTerm(con, loop_term.Fst, loop_term.Snd)
+		})
+	}
+
+	for _, ty := range tys.GetSlice() {
+		loop_ty := ty
+		calls = append(calls, func(outchan chan bool) {
+			outchan <- typecheckType(con, loop_ty)
+		})
+	}
+
+	res, err := Lib.GenericParallel(
+		calls,
+		func(x, y bool) bool { return x && y },
+		true, // Neutral of the operation
+	)
+
+	if err != nil {
+		Glob.Anomaly(
+			label,
+			fmt.Sprintf("Encountered a Lib error: %s", err.Error()),
+		)
+	}
+
+	return res
+}
+
+func buildTermCheckList(
+	debug_str Lib.Lazy[string],
+	ty AST.Ty,
+	terms Lib.List[AST.Term],
+	tys Lib.List[AST.Ty],
+) Lib.List[Lib.Pair[AST.Term, AST.Ty]] {
+	if terms.Len() != tys.Len() {
+		Glob.Fatal(
+			label,
+			fmt.Sprintf("Expected %d arguments in %s (which has type %s), but got %d",
+				tys.Len(), debug_str.Run(), ty.ToString(), terms.Len()),
+		)
+		return Lib.NewList[Lib.Pair[AST.Term, AST.Ty]]()
+	}
+
+	ls := Lib.MkList[Lib.Pair[AST.Term, AST.Ty]](terms.Len())
+	for i := range terms.GetSlice() {
+		ls.Upd(i, Lib.MkPair(terms.At(i), tys.At(i)))
+	}
+	return ls
 }
